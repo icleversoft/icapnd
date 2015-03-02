d6ecb1cd2ad6e43f00ed4003ab3116fb0678651a
George Stavrou <iphone@icleversoft.com>
2015-03-02 19:48:57 +0200

    perform updates according to APL's latest documentation

--- lib/icapnd/notification.rb
+++ lib/icapnd/notification.rb
@@ -1,28 +1,52 @@
 require 'yajl' unless defined?(Yajl)
 
 module Icapnd
+  #http://apple.co/17IAp6G
   class Notification
+    PAYLOAD_MAX_PRIOR_IOS8 = 256
+    PAYLOAD_MAX = 2 * 1024
 
     attr_accessor :device_token, :alert, :badge, :sound, :custom
-
-    PAYLOAD_MAX_BYTES = 256
+    attr_reader :payload
     class PayloadTooLarge < StandardError;end
     class NoDeviceToken < StandardError;end
 
+    def initialize( before_ios8 = true )
+      @max_payload_size = before_ios8 == true ? PAYLOAD_MAX_PRIOR_IOS8 : PAYLOAD_MAX
+      @payload = {aps:{}}
+    end
+
+    def add_to_payload( hash )
+      @payload.merge!( hash ) if hash.is_a?(Hash)
+      @payload
+    end
+
+    def badge=(value)
+      @payload[:aps][:badge] = value.to_i
+      @payload
+    end
+
+    def alert=(value)
+      @payload[:aps][:alert] = value
+      @payload
+    end
+
+    def sound=(value)
+      @payload[:aps][:sound] = value
+      @payload
+    end
+
+    def force_silent
+      @silent = true
+      @payload[:aps]['content-available'] = 1
+    end
+
     def encode_payload
-      p = {:aps => Hash.new}
-      [:badge, :alert, :sound].each do |k|
-        p[:aps][k] = send(k) if send(k)
-      end
-      p.merge!(custom) if send(:custom)
-
-      j = Yajl::Encoder.encode(p)
-      raise PayloadTooLarge.new("The payload is larger than allowed: #{j.length}") if j.size > PAYLOAD_MAX_BYTES
-
-      p[:device_token] = device_token
       raise NoDeviceToken.new("No device token") unless device_token
-
-      Yajl::Encoder.encode(p)
+      @payload[:device_token] = device_token
+      j = Yajl::Encoder.encode( @payload )
+      raise PayloadTooLarge.new("The payload length: #{j.length} is larger than allowed: #{@max_payload_size}") if j.size > @max_payload_size
+      Yajl::Encoder.encode(@payload)
     end
 
     def push
@@ -38,7 +62,7 @@ module Icapnd
       raise NoDeviceToken.new("No device token") unless device_token
 
       j = Yajl::Encoder.encode(notif_hash)
-      raise PayloadTooLarge.new("The payload is larger than allowed: #{j.length}") if j.size > PAYLOAD_MAX_BYTES
+      raise PayloadTooLarge.new("The payload length: #{j.length} is larger than allowed: #{@max_payload_size}") if j.size > @max_payload_size
 
       Config.logger.debug "TOKEN:#{device_token} | ALERT:#{notif_hash.inspect}"
 
